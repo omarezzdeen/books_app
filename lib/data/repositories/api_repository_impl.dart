84910e75fa7a6351e3538aa23080604378a7adf1
omar ezzdeen <omar.izziddeen@gmail.com>
2023-03-17 19:06:30 +0200

    create architecture

---
+++ lib/data/repositories/api_repository_impl.dart
@@ -0,0 +1,9 @@
+
+
+import 'package:books_app/data/repositories/base/base_api_repository.dart';
+
+import '../../domain/repositories/api_repository.dart';
+
+class ApiRepositoryImpl extends BaseApiRepository implements ApiRepository {
+
+}
---
+++ lib/data/repositories/database_repository_impl.dart
@@ -0,0 +1,6 @@
+
+import '../../domain/repositories/database_repository.dart';
+
+class DatabaseRepositoryImpl implements DatabaseRepository {
+
+}
